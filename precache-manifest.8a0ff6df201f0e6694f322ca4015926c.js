4ac1c2cb4e1e7807e69699f6d204f358fbf7e44d
Soloman Garcia <soloman.garcia@gmail.com>
2020-02-27 09:08:49 -0700

    Updates

---
+++ precache-manifest.8a0ff6df201f0e6694f322ca4015926c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "981751c70995618ec70836dbb0e49d28",
+    "url": "/expense-tracker-react/index.html"
+  },
+  {
+    "revision": "8973a76d40ee4f15875f",
+    "url": "/expense-tracker-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "11a231404ae03f238e81",
+    "url": "/expense-tracker-react/static/js/2.31c886ff.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/expense-tracker-react/static/js/2.31c886ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8973a76d40ee4f15875f",
+    "url": "/expense-tracker-react/static/js/main.657bda5c.chunk.js"
+  },
+  {
+    "revision": "b64b2e679878f9878781",
+    "url": "/expense-tracker-react/static/js/runtime-main.2147a7c9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/expense-tracker-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
