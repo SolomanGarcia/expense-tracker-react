2ac8d3987af3b050f28e8a5e8ab285e3440ba55d
Soloman Garcia <soloman.garcia@gmail.com>
2020-02-28 13:30:34 -0700

    add useEffect hook to transactionList.js

--- client/src/context/GlobalState.js
+++ client/src/context/GlobalState.js
@@ -49,6 +49,9 @@ export const GlobalProvider = ({ children }) => {
 
     return (<GlobalContext.Provider value={{
         transactions: state.transactions,
+        error: state.error,
+        loading: state.loading,
+        getTransactions,
         deleteTransaction,
         addTransaction
     }}>
